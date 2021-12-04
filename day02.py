1e0e651feaad428a0d3a59afe7e6db0d89e444a8
Thom Dickson <td3of4@gmail.com>
2021-12-04 15:47:33 -0500

    Solve day02 part 2

--- day02.py
+++ day02.py
@@ -10,3 +10,20 @@ with open("day02.in") as f:
 
 print("Current position:", pos)
 print("Product:", pos[0] * pos[1])
+
+# Part 2
+pos = [0, 0, 0]
+with open("day02.in2") as f:
+    for x in f:
+        cmd = x.split()[0]
+        val = int(x.split()[1])
+        if cmd == "down":
+            pos[2] += val
+        elif cmd == "up":
+            pos[2] -= val
+        elif cmd == "forward":
+            pos[0] += val
+            pos[1] += pos[2] * val
+
+print("Current position:", pos)
+print("Product:", pos[0] * pos[1])
